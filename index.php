9e096331ddcbdadfa5c2e0c58d32fe830eea601c
Carlos Bautista <cbautista2013@gmail.com>
2018-06-07 06:31:24 -0700

    added css and form in index

--- index.php
+++ index.php
@@ -3,27 +3,58 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Shout It! </title>
-    <link rel="stylesheet" href="css/style.css" type="text/css" />-
+    <title> SHOUT IT! </title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 
 <body>
     <div id="container">
         <header>
-            <h1>SHOUT IT ! Shoutbox </h1>
+            <h1>SHOUT IT! Shoutbox </h1>
         </header>
         <div id="shouts">
 
             <ul>
-                <li class = "shout" >
-                
-                <span>10:19 PM</span>
-                
+                <li class="shout">
+
+                    <span>10:19 PM</span>
+                    Brad: Hey what are you guys up to?
+                </li>
+                <li class="shout">
+
+                    <span>10:19 PM</span>
+                    Brad: Hey what are you guys up to?
+                </li>
+                <li class="shout">
+
+                    <span>10:19 PM</span>
+                    Brad: Hey what are you guys up to?
+                </li>
+                <li class="shout">
+
+                    <span>10:19 PM</span>
+                    Brad: Hey what are you guys up to?
+                </li>
+                <li class="shout">
+
+                    <span>10:19 PM</span>
+                    Brad: Hey what are you guys up to?
                 </li>
             </ul>
 
         </div>
+        <div id="input">
 
+            <form method="post" action="proccess.php">
+                <input type="text" name="user" placeholder="Enter Your Name" />
+                <input type="text" name = "message" placeholder = "Enter A Message" />
+                <br/>
+                <input class = "shout-btn" type="submit" name = "submit" value = "Shout It Out">
+
+            </form>
+
+
+        </div>
     </div>
 </body>
 
