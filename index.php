42164bdd9516b011d1e739d19f56ba642017e05f
Carlos Bautista <cbautista2013@gmail.com>
2018-06-07 03:08:32 -0700

    Shout it first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Shout It! </title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />-
+</head>
+
+<body>
+    <div id="container">
+        <header>
+            <h1>SHOUT IT ! Shoutbox </h1>
+            <ul>
+                <li></li>
+            </ul>
+        </header>
+    </div>
+</body>
+
+</html>
